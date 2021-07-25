962c7147dea7ca78b26bbacde63355ffd291313d
Discord0000 <78850850+Discord0000@users.noreply.github.com>
2021-07-25 23:54:10 +0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,1 +1,11 @@
-print("oof")
+local discordinv = "https://discord.gg/tgh3dtSEW7"
+local d
+local f = pcall(function()
+    d = game:HttpGet("https://raw.githubusercontent.com/Discord0000/Zer0Hub/main/GameScripts/"..game.PlaceId..".lua")
+end)
+if f == true then
+    loadstring(d)()
+else
+  game.Players.LocalPlayer:Kick("Join Discord :D link copied. "..discordinv)
+  setclipboard(discordinv)
+end
